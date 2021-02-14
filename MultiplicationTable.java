23bd12c3f724c70a8792d7f45797eabcdfe81eb7
OliverAltaras <78381806+OliverAltaras@users.noreply.github.com>
2021-02-14 13:43:47 +0000

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,35 @@
+//Application that prints a multiplication table for numbers up to 12
+
+//importing the java.util package to use the Scanner class
+import java.util.*;
+
+public class MultiplicationTable {
+
+	// Scanner class used for user input
+	Scanner scan = new Scanner(System.in);
+
+	// method returns an int, which is then passed as an argument in table()
+	public int userInput() {
+		int input = scan.nextInt();
+		return input;
+	}
+
+	// method generates the product of the iteration int and the user input
+	public static void table(int input) {
+		for (int i = 1; i <= 12; i++) {
+			System.out.println(i + " * " + input + " = " + (i * input));
+		}
+	}
+
+	// main method
+	public static void main(String[] args) {
+
+		// creating an instance of MultiplicationTable
+		MultiplicationTable mt = new MultiplicationTable();
+
+		// invoking both methods
+		System.out.println("Please enter the number you'd like to multiply> ");
+		int input = mt.userInput();
+		table(input);
+	}
+}
